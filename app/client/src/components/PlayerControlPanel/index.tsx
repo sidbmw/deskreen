74ac1a971b9ff028aa20d61753f04cbc3dfae154
sidbmw <siddharthnatamai@gmail.com>
2021-04-08 16:14:47 -0400

    fps

--- app/client/src/components/PlayerControlPanel/index.tsx
+++ app/client/src/components/PlayerControlPanel/index.tsx
@@ -166,6 +166,12 @@ function PlayerControlPanel(props: PlayerControlPanelProps) {
               </Button>
             </Tooltip>
           </Col>
+          <Col xs={12} md={1}>
+            <Row center="xs" style={{ height: '42px' }}>
+              <Text>FPS:&nbsp;</Text>
+              <p id="fps-show"></p>
+            </Row>
+          </Col>
           <Col xs={12} md={5}>
             <Row center="xs" style={{ height: '42px' }}>
               <Row
